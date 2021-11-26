a0bcca6fa472f08ea0280055e2b13bc53303356b
NikolaiRozhkov <agent-nik126@mail.ru>
2021-11-26 14:59:05 +0300

    without drag/drop

--- scripts/controller/control.js
+++ scripts/controller/control.js
@@ -1,28 +1,38 @@
 import View from "../views/view.js";
 import Task from "../models/model.js";
 
+
 const ulElement = document.querySelector('.list');
 const divLi = document.querySelector('.Li');
 const input = document.querySelector('.newTask');
 
 
 function App() {
-    this.taskList = [];
+    this.taskList = [...this.LocalStorageTasks()];
+    this.renderList = this.LocalStorageRenderList();
+    this.completedList = [...this.LocalStorageCompletedList()];
+    this.activeList = [...this.LocalStorageActiveList()]
     this.view = new View (ulElement, divLi, {
         onDeleting: (function (id) {this.deleting(id)}).bind(this),
         onEditing: (function (id) {this.editing(id)}).bind(this),
-        backForLabel: (function (valueInput, index) {this.returnForLabel(valueInput, index)}).bind(this),
-        onChecking: (function (index) {this.checking(index)}).bind(this)
+        backForLabel: (function (valueInput, id) {this.returnForLabel(valueInput, id)}).bind(this),
+        onChecking: (function (id) {this.checking(id)}).bind(this),
+        onCheckingAll: (function () {this.checkAllTasks()}).bind(this),
+        onClrCompleted: (function () {this.clearCompletedTasks()}).bind(this),
+        onActive: (function () {this.showActive()}).bind(this),
+        showCompletedTaskList: (function () {this.completedTaskList()}).bind(this),
     })
 };
 
 App.prototype.init = function () {
+    this.LocalStorageSetRender();
     input.addEventListener('keydown', function (e) {
         if (e.key === "Enter" && !(/^ *$/.test(input.value))) {
             this.addTask(e.target.value);
             input.value = '';
         };
     }.bind(this));
+    this.clickFilters();
 };
 
 App.prototype.addTask = function (inputVal) {
@@ -30,7 +40,7 @@ App.prototype.addTask = function (inputVal) {
     const task = new Task(Date.now(), inputVal);
     this.taskList.push(task);
     let lengthTaskList = this.taskList.length;
-    this.view.render(this.taskList, ulElement);
+    this.LocalStorageSetRender();
 };
 
 App.prototype.deleting = function (id) {
@@ -38,24 +48,151 @@ App.prototype.deleting = function (id) {
         return task.id === id;
     });
     this.taskList.splice(task, 1);
-    this.view.render(this.taskList);
+    this.LocalStorageSetRender();
 };
 
-App.prototype.editing = function (index) {
-    let val = this.taskList[index].value;
-    this.view.onEditing(val, index);
+App.prototype.editing = function (id) {
+    const task = this.taskList.find(function (task) {
+        return task.id === id;
+    });
+    let val = task.value;
+    this.view.onEditing(val, task.id);
 };
 
-App.prototype.checking = function (index) {
+App.prototype.checking = function (id) {
+    const index = this.taskList.findIndex(function (task) {
+        return task.id === id;
+    });
     this.taskList[index].completed = !this.taskList[index].completed;
-    this.view.onChecking(index, this.taskList[index].completed, this.taskList);
-    console.log(this.taskList);
+    this.LocalStorageSetRender();
 };
 
-App.prototype.returnForLabel = function (valueInput, index) {
-    this.taskList[index].value = valueInput;
-    this.view.render(this.taskList);
+App.prototype.checkAllTasks = function () {
+    if (this.taskList != []) {
+        this.taskList.forEach(function (task) {
+            task.completed = true;
+        });
+    }
+    
+    this.LocalStorageSetRender();
 };
+
+
+App.prototype.returnForLabel = function (valueInput, id) {
+    const task = this.taskList.find(function (task) {
+        return task.id === id;
+    });
+    task.value = valueInput;
+    if (task.value == '') {
+        this.deleting(id);
+    }
+    this.LocalStorageSetRender();
+};
+
+App.prototype.clearCompletedTasks = function () {
+    this.taskList = this.taskList.filter(function (task) {
+        if (task.completed === false) {
+            return true;
+        }
+    });
+    this.LocalStorageSetRender();
+};
+
+
+//  LOCAL STORAGE
+
+
+App.prototype.LocalStorageCompletedList = function() { 
+    return (JSON.parse(localStorage.getItem('completedList')))
+}
+
+App.prototype.LocalStorageActiveList = function() { 
+    return (JSON.parse(localStorage.getItem('activeList'))) || []
+}
+
+App.prototype.LocalStorageTasks = function() { 
+    return (JSON.parse(localStorage.getItem('taskList'))) || []
+}
+
+App.prototype.LocalStorageRenderList = function() {
+    return (JSON.parse(localStorage.getItem('renderList'))) || 'All'
+}
+
+App.prototype.showActive = function() {
+    return this.taskList.filter(function(task) {
+        return !task.completed;
+    })
+}
+
+App.prototype.showAll = function() {
+    let allTasks = this.taskList;
+    return allTasks;
+}
+
+App.prototype.showCompleted = function() {
+    return this.taskList.filter(function(task) {
+        return task.completed;
+    })
+}
+
+App.prototype.LocalStorageSetRender = function() {
+    document.querySelector('.footer').setAttribute('style', 'display: flex;');
+    const liAll = document.querySelector('#liAll');
+    const liActive = document.querySelector('#liActive');
+    const liCompleted = document.querySelector('#liCompleted');
+    let completedList = this.showCompleted();
+    let activeList = this.showActive();
+    let allTasks = this.showAll();
+    let listRender = [];
+    if(this.renderList === 'Active') {
+        listRender = [...activeList];
+        liAll.classList.remove('selected');
+        liCompleted.classList.remove('selected');
+        liActive.classList.add('selected');
+    } else if (this.renderList === 'Completed') {
+        listRender = [...completedList];
+        liActive.classList.remove('selected');
+        liAll.classList.remove('selected');
+        liCompleted.classList.add('selected');
+    } else {
+        listRender = [...allTasks];
+        liActive.classList.remove('selected');
+        liCompleted.classList.remove('selected');
+        liAll.classList.add('selected');
+    }
+    
+    localStorage.setItem('activeList', JSON.stringify(activeList))
+    localStorage.setItem('renderList', JSON.stringify(this.renderList))
+    localStorage.setItem('taskList', JSON.stringify(allTasks))
+    localStorage.setItem('completedList', JSON.stringify(completedList))
+    
+    if (allTasks < 1) {
+        document.querySelector('.footer').removeAttribute('style', 'display: flex;');
+    }
+
+    this.view.render(listRender);
+}
+
+App.prototype.clickFilters = function() {
+    const filtersButtons = document.querySelector('.filters');
+    filtersButtons.addEventListener('click', function(e) {
+        console.log(filtersButtons);
+        if(e.target.id === 'aActive') {
+            this.renderList = "Active"
+            console.log('Active:', this.renderList);
+        } else if(e.target.id === 'aAll') {
+            this.renderList = 'All'
+            console.log('All:', this.renderList);
+        } else if (e.target.id === 'aCompleted') {
+            this.renderList = "Completed"
+            console.log('Сompleted:', this.renderList);
+        }
+        console.log(this.renderList);
+        this.LocalStorageSetRender();
+
+    }.bind(this))
+}
+
 
 
 export default App;
--- scripts/views/view.js
+++ scripts/views/view.js
@@ -7,20 +7,23 @@ function View(ulElement, divLi, actions) {
     this.editingListener = actions.onEditing;
     this.returnForLable = actions.backForLabel;
     this.checkTask = actions.onChecking;
+    this.checkAllTasks = actions.onCheckingAll;
+    this.clearCompletedTasks = actions.onClrCompleted;
+    this.activeTaskList = actions.onActive;
+    this.completedTaskList = actions.showCompletedTaskList;
 }
 
 View.prototype.render = function (taskList = []) {
-    this.clear();
+
+    this.divLi.replaceChildren();
     
     let lengthTaskList = taskList.length;
 
     const fragment = document.createDocumentFragment();
 
+    let completed = 0;
+
     taskList.forEach(function (task) {
-        let id = task.id;
-        let index = taskList.findIndex(function (task) {
-            return task.id === id;
-        });
 
         let li = document.createElement('li');
 
@@ -32,29 +35,48 @@ View.prototype.render = function (taskList = []) {
 
         let inpCheck = document.createElement('input');
         inpCheck.classList.add('toggle');
-        inpCheck.setAttribute('id', `${index + 'ch'}`);
+        inpCheck.setAttribute('id', `${task.id + 'ch'}`);
         inpCheck.setAttribute('type', 'checkbox');
-        inpCheck.addEventListener('click', function () {
-            this.checkTask(index);
+        inpCheck.addEventListener('click', function (e) {
+            e.preventDefault();
+            this.checkTask(task.id);
         }.bind(this));
 
         let label = document.createElement('label');
         label.classList.add('label-text');
-        label.setAttribute('id', `${index}`);
-        label.setAttribute('for', `${index + 'ch'}`);
+        label.setAttribute('id', `${String(task.id)}`);
+        label.setAttribute('for', `${task.id + 'ch'}`);
         label.insertAdjacentText('afterbegin', `${task.value}`);
+
+        if (task.completed) {
+            let onCheking = function () {
+                label.setAttribute('style', 'text-decoration: line-through;');
+                inpCheck.checked = true;
+            }
+            onCheking();
+        }
+        else if (label.hasAttribute('style')) {
+            let delAttribute = function () {
+                label.removeAttribute('style');
+                inpCheck.checked = false;
+            }
+            delAttribute();
+        }
+
 
         let inputTsk = document.createElement('input');
         inputTsk.classList.add('task-text');
-        inputTsk.setAttribute('id', `${index + 'i'}`);
+        inputTsk.setAttribute('id', `${task.id + 'i'}`);
         inputTsk.setAttribute('style', 'border: none;');
         inputTsk.setAttribute('maxlength', '33');
         inputTsk.addEventListener('keydown', function (e) {
             if (e.key === 'Enter') {
-                this.returnForLable(inputTsk.value, index);
+                console.log(inputTsk.value);
+                this.returnForLable(inputTsk.value, task.id);
             };
         }.bind(this));
-
+        
+    
         let divButton = document.createElement('div');
         divButton.classList.add('button-edit');
 
@@ -63,7 +85,7 @@ View.prototype.render = function (taskList = []) {
         let iconEdit = document.createElement('i');
         iconEdit.className = 'far fa-edit';
         buttonEdit.addEventListener('click', function () {
-            this.editingListener(index);
+            this.editingListener(task.id);
         }.bind(this));
 
         let buttonDelete = document.createElement('button');
@@ -71,7 +93,14 @@ View.prototype.render = function (taskList = []) {
         let iconDelete = document.createElement('i');
         iconDelete.className = 'fas fa-times';
         buttonDelete.addEventListener('click', function () {
-            this.deletingListener(task.id);
+            const deleteTask = this.deletingListener;
+
+            this.createModal().then(function () {
+                deleteTask(task.id);
+                document.querySelector('#popup').removeAttribute('style');
+            }).catch(function(e) {
+                document.querySelector('#popup').removeAttribute('style');
+            });
         }.bind(this)); 
         
         buttonDelete.append(iconDelete);
@@ -81,105 +110,70 @@ View.prototype.render = function (taskList = []) {
         divView.append(divDescription, divButton);
         li.append(divView);
         fragment.appendChild(li);
-        
+
+        if (task.completed == true) {
+            completed += 1;
+        }
+
     }.bind(this));
 
-    this.divLi.appendChild(fragment);
+    document.querySelector('.Li').appendChild(fragment);
+    
+    if (completed > 0) {
+        document.querySelector('.counter').textContent  = lengthTaskList - completed;
+    }
+    else {
+        document.querySelector('.counter').textContent = lengthTaskList;
+    }
 
-
-    // Footer
-
-    let clearFooter = function () {
-        if (document.querySelector('.footer')) {
-            (document.querySelector('.list')).removeChild((document.querySelector('.footer')));
+    let that = this;
+    document.querySelector('.clear-completed').addEventListener('click', function () {
+        if (completed > 0) {
+            that.createModal().then(function () {
+                that.clearCompletedTasks();
+                document.querySelector('#popup').removeAttribute('style');
+            }).catch(function(e) {
+                document.querySelector('#popup').removeAttribute('style');
+            });
         }
-    };
-
-    if (lengthTaskList > 0) {
         
-        clearFooter();
+    });
 
-        let divFooter = document.createElement('div');
-        divFooter.classList.add('footer');
+    document.querySelector('#completeAll').addEventListener('click', function() {
+        if (taskList != []) {
+            this.checkAllTasks(completed);
+        }
+    }.bind(this)); 
+};
 
-        let spanDescript = document.createElement('span');
-        spanDescript.classList.add('todo-count');
-        spanDescript.insertAdjacentText('beforeend', 'items left');
-        let spanCount = document.createElement('span');
-        spanCount.classList.add('counter');
-        spanCount.insertAdjacentText('afterbegin', `${lengthTaskList}`);
+View.prototype.createModal = function () {
+    document.querySelector('#popup').setAttribute('style', 'visibility: visible;');
+    const buttonDelete = document.querySelector('#buttonDelete');
+    const buttonCancel = document.querySelector('#buttonCancel');
 
-        let ulFilters = document.createElement('ul');
-        ulFilters.classList.add('filters');
-
-        let liSelected = document.createElement('li');
-        liSelected.classList.add('filter');
-        let aSelected = document.createElement('a');
-        aSelected.classList.add('selected');
-        aSelected.setAttribute('href', '#');
-        aSelected.insertAdjacentText('beforeend', 'All');
-        liSelected.append(aSelected);
-
-        let liActive = document.createElement('li');
-        liActive.classList.add('filter');
-        let aActive = document.createElement('a');
-        aActive.setAttribute('href', '#');
-        aActive.insertAdjacentText('beforeend', 'Active');
-        liActive.append(aActive);
-
-        let liCompleted = document.createElement('li');
-        liCompleted.classList.add('filter');
-        let aCompleted = document.createElement('a');
-        aCompleted.setAttribute('href', '#');
-        aCompleted.insertAdjacentText('beforeend', 'Completed');
-        liCompleted.append(aCompleted);
-
-        let buttonClrCompleted = document.createElement('button');
-        buttonClrCompleted.classList.add('clear-completed');
-        buttonClrCompleted.insertAdjacentText('beforeend', 'Clear completed');
-
-        ulFilters.append(liSelected);
-        ulFilters.append(liActive);
-        ulFilters.append(liCompleted);
-        divFooter.append(spanCount, spanDescript);
-        divFooter.append(ulFilters);
-        divFooter.append(buttonClrCompleted);
-
-
-        this.element.append(divFooter);
-    }
-    else if (lengthTaskList < 1) {
-        clearFooter();
-    }
+    return new Promise(function(resolve, reject) {
+        buttonDelete.addEventListener("click", function() {
+            resolve()
+        })
+        buttonCancel.addEventListener("click", function() {
+            reject()
+        })
+    })
 
 };
 
-View.prototype.onEditing = function (val, index) {
-    let label = document.querySelector(`[id=${CSS.escape(index)}]`);
+
+View.prototype.onEditing = function (val, id) {
+    let label = document.querySelector(`[id=${CSS.escape(id)}]`);
     label.classList.remove('label-text');
     label.classList.add('hidden-label');
     let labelValue = val;
-    let input = document.querySelector(`[id=${CSS.escape(index + 'i')}`);
+    let input = document.querySelector(`[id=${CSS.escape(id + 'i')}`);
     input.classList.remove('task-text');
     input.classList.add('label-text');
     input.value = labelValue;
     input.focus();
 };
 
-View.prototype.onChecking = function (index, completed) {
-    if (completed) {
-        console.log(1);
-        (document.querySelector(`[id=${CSS.escape(index)}]`)).setAttribute('style', 'text-decoration: line-through;');
-    }
-    else {
-        (document.querySelector(`[id=${CSS.escape(index)}]`)).removeAttribute('style');
-    }
-
-    return 'ok';
-};
-
-View.prototype.clear = function() {
-    this.divLi.replaceChildren();
-};
 
 export default View;
